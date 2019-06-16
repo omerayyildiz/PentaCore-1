ecf36bd363aa3286dbbd9985fd355f83fbf46eb7
femrek <faruk.eksioglu@hotmail.com>
2019-06-16 16:45:05 +0300

    dgl kullanıldı

--- app/src/main/java/com/eksioglu/faruk/elementler/IletisimDialog.java
+++ app/src/main/java/com/eksioglu/faruk/elementler/IletisimDialog.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
+import android.support.annotation.Px;
+import android.support.v4.util.LogWriter;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.eksioglu.faruk.divisiblegridlayout.DivisibleGridLayout;
 
 import static com.eksioglu.faruk.elementler.MainActivity.weight;
 
@@ -19,7 +23,7 @@ public class IletisimDialog extends Dialog {
 
     Context context;
 
-    private ConstraintLayout constraintLayout;
+    private DivisibleGridLayout divisibleGridLayout;
     private ImageView imageWeb, imageGithub, imageDiscord, imageLinkedin;
     private TextView textWeb, textGithub, textDiscord, textLinkedin;
 
@@ -34,7 +38,7 @@ public class IletisimDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.iletisim_dialog);
         getWindow().setBackgroundDrawable(new ColorDrawable(0x00000000));
-        constraintLayout = findViewById(R.id.iletisimDialog_ConstraintLayout);
+        divisibleGridLayout = findViewById(R.id.iletisimDialog_ConstraintLayout);
         imageWeb      = findViewById(R.id.iletisimImage_web);
         imageGithub   = findViewById(R.id.iletisimImage_github);
         imageDiscord  = findViewById(R.id.iletisimImage_discord);
@@ -56,7 +60,23 @@ public class IletisimDialog extends Dialog {
         imageLinkedin.setOnClickListener(onLinkedinClickListener);
         textLinkedin .setOnClickListener(onLinkedinClickListener);
 
-        setViewSize(constraintLayout, weight/6*5, 0);
+        int layoutWidth = weight/6*5;
+        int dividerWeight = layoutWidth/40;
+        setViewSize(divisibleGridLayout, layoutWidth, 0);
+        divisibleGridLayout.setAllViewsSizePx(layoutWidth/2-dividerWeight/2*3, layoutWidth/20*11);
+        divisibleGridLayout.setPadding(dividerWeight, dividerWeight, dividerWeight, dividerWeight);
+        divisibleGridLayout.setDividerWeightPx(dividerWeight);
+
+        textDiscord.setTextSize(TypedValue.COMPLEX_UNIT_PX, ((float)layoutWidth)/25);
+        textWeb.setTextSize(TypedValue.COMPLEX_UNIT_PX, ((float)layoutWidth)/25);
+        textGithub.setTextSize(TypedValue.COMPLEX_UNIT_PX, ((float)layoutWidth)/25);
+        textLinkedin.setTextSize(TypedValue.COMPLEX_UNIT_PX, ((float)layoutWidth)/25);
+
+        textDiscord.setPadding(0, layoutWidth/50, 0, layoutWidth/50);
+        textWeb.setPadding(0, layoutWidth/50, 0, layoutWidth/50);
+        textGithub.setPadding(0, layoutWidth/50, 0, layoutWidth/50);
+        textLinkedin.setPadding(0, layoutWidth/50, 0, layoutWidth/50);
+
     }
 
     private View.OnClickListener onWebClickListener = new View.OnClickListener() {
